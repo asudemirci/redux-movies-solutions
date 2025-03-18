c37b3a9f5d4bcd454cd35e8fe83d9a47d8bc1809
asude-demirci <asudemrci@gmail.com>
2025-03-18 12:34:03 +0300

    feat: fetch displayFavorites from Redux with useSelector

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,13 @@ import MovieHeader from './components/MovieHeader';
 
 import AddMovieForm from './components/AddMovieForm';
 import FavoriteMovieList from './components/FavoriteMovieList';
+import { useSelector } from 'react-redux';
+
 
 const App = (props) => {
-  const displayFavorites = true;
+  const displayFavorites = useSelector(
+    (store) => store.favorites.displayFavorites
+  );;
 
   return (
     <div>
